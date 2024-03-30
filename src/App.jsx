f9599fd6f903dbf4c89f231f99f043166c2dd780
Joshuacodeat10 <joshuaagboola99@gmail.com>
2024-03-30 22:24:11 +0100

    added more styles
    styled grid more

--- src/App.jsx
+++ src/App.jsx
@@ -50,24 +50,26 @@ function App() {
       <div className="container">
        
         <div className="calculatorapp" onKeyPress={(e) => {if(e.key === "Enter" || e.code==="Space"){ submitInput(event);}}}> 
-          <form className="calculator" >
-          <input type="text" value={calculatorInput} onChange={saveInput} autoFocus />
-          <button onClick={() => {addOperator(event, '+')}} onKeyPress={preventDefault}>+</button>
-          <button onClick={() => {addOperator(event, '-')}} onKeyPress={preventDefault}>-</button>
-          <button onClick={() => {addOperator(event, '*')}} onKeyPress={preventDefault}>*</button>
-          <button onClick={() => {addOperator(event, '/')}} onKeyPress={preventDefault}>÷</button>
-          <button onClick={() => {addDigit(event, '1')}}  onKeyPress={preventDefault}>1</button>
-          <button onClick={() => {addDigit(event, '2')}} onKeyPress={preventDefault}>2</button>
-          <button onClick={() => {addDigit(event, '3')}} onKeyPress={preventDefault}>3</button>
-          <button onClick={() => {addDigit(event, '4')}} onKeyPress={preventDefault}>4</button>
-          <button onClick={() => {addDigit(event, '5')}} onKeyPress={preventDefault}>5</button>
-          <button onClick={() => {addDigit(event, '6')}} onKeyPress={preventDefault}>6</button>
-          <button onClick={() => {addDigit(event, '7')}} onKeyPress={preventDefault}>7</button>
-          <button onClick={() => {addDigit(event, '8')}} onKeyPress={preventDefault}>8</button>
-          <button onClick={() => {addDigit(event, '9')}} onKeyPress={preventDefault}>9</button>
-          <button onClick={deleteInput}>del</button>
-          <button onClick={clearInput}>AC</button>
-          <button onClick={submitInput}type="submit">=</button>
+          <form  >
+            <input type="text" value={calculatorInput} onChange={saveInput} autoFocus />
+            <div className="calculator">
+              <button onClick={() => {addOperator(event, '+')}} onKeyPress={preventDefault}>+</button>
+              <button onClick={() => {addOperator(event, '-')}} onKeyPress={preventDefault}>-</button>
+              <button onClick={() => {addOperator(event, '*')}} onKeyPress={preventDefault}>*</button>
+              <button onClick={() => {addOperator(event, '/')}} onKeyPress={preventDefault}>÷</button>
+              <button onClick={() => {addDigit(event, '1')}}  onKeyPress={preventDefault}>1</button>
+              <button onClick={() => {addDigit(event, '2')}} onKeyPress={preventDefault}>2</button>
+              <button onClick={() => {addDigit(event, '3')}} onKeyPress={preventDefault}>3</button>
+              <button onClick={() => {addDigit(event, '4')}} onKeyPress={preventDefault}>4</button>
+              <button onClick={() => {addDigit(event, '5')}} onKeyPress={preventDefault}>5</button>
+              <button onClick={() => {addDigit(event, '6')}} onKeyPress={preventDefault}>6</button>
+              <button onClick={() => {addDigit(event, '7')}} onKeyPress={preventDefault}>7</button>
+              <button onClick={() => {addDigit(event, '8')}} onKeyPress={preventDefault}>8</button>
+              <button onClick={() => {addDigit(event, '9')}} onKeyPress={preventDefault}>9</button>
+              <button onClick={deleteInput}>del</button>
+              <button onClick={clearInput}>AC</button>
+              <button onClick={submitInput}type="submit">=</button>
+            </div>
           </form>
           
          
