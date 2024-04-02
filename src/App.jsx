36bba0cdf090b3ba0fb85d0ba9ae6fb01a637b57
Joshuacodeat10 <joshuaagboola99@gmail.com>
2024-04-02 20:11:10 +0100

    Updated styles, Worked on validation.

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,18 @@ function App() {
   //  use state to set input basically where the calculator will be displayed,
   //  where you see the digits
   const [calculatorInput, setCalculatorInput] = useState(''); 
-  
    const saveInput = (event) =>{
-    const regex =/^[\d%.*/+\-]+$/;
-    const conditions=["++","**","*+"];
-   
-    const userInput = event.target.value;
-    // const previousCharacter= calculatorInput[calculatorInput.length-1];
-  //  console.log(previousCharacter)
-    if(regex.test(userInput) && conditions.every(check => !userInput.includes(check))){
-      setCalculatorInput(userInput)
-      console.log('please set')
+      const regex =/^[\d%.*/+\-]+$/;
+      const conditions=["++","**","*+"];
+    
+      const userInput = event.target.value;
+      // const previousCharacter= calculatorInput[calculatorInput.length-1];
+    //  console.log(previousCharacter)
+      if(regex.test(userInput) && conditions.every(check => !userInput.includes(check))){
+        setCalculatorInput(userInput)
+        console.log('please set')
 
-    } 
+      } 
  
     // if( previousCharacter.includes('+')){
     //  console.log('block')
@@ -28,40 +27,45 @@ function App() {
   }
   const submitInput =(event) =>{
     event.preventDefault();
-    const newValue= eval(calculatorInput);
-    setCalculatorInput(newValue.toString())
+    try{
+      const newValue= eval(calculatorInput);
+    setCalculatorInput(newValue)
+    }
+    catch{
+      setCalculatorInput('ERROR')
+    }
   } 
   const clearInput=(event)=>{
     event.preventDefault();
-    setCalculatorInput('')
-    
-    
+    setCalculatorInput('') 
   }
-
   const preventDefault=(e) => {
      e.preventDefault();  
     }
   const deleteInput = (event) => {
     event.preventDefault();
     if (!calculatorInput) return; // Handle empty string case
-  
-   setCalculatorInput(calculatorInput.slice(0, calculatorInput.length - 1));
+    const calculatorLength=calculatorInput.length;
+    setCalculatorInput(calculatorInput.slice(0, calculatorInput.length - 1));
   };
-
   const addDigit = (event, value) => {
     event.preventDefault();
     event.stopPropagation();
-
     setCalculatorInput(calculatorInput + value);
   }
   const addOperator = (event, value) => {
     event.preventDefault();
     event.stopPropagation();
+    const conditions=["+","*","/"];
+    
     const previousCharacter= calculatorInput[calculatorInput.length-1];
-    if(previousCharacter.includes('+','*','/')){
-      value='';
+    const currentCharacter= calculatorInput[calculatorInput.length];
+    console.log(conditions)
+    console.log(previousCharacter)
+    if(conditions.some(check => previousCharacter.includes(check))){
+      value=''
     }
-    setCalculatorInput(calculatorInput + value);
+  setCalculatorInput(calculatorInput + value);
   }
   return (
       <div className="container">
@@ -71,22 +75,35 @@ function App() {
             <input type="text" value={calculatorInput} onChange={saveInput} autoFocus />
             <div className="buttons">
               <div className="inner">
-                <button onClick={() => {addOperator(event, '+')}} onKeyPress={preventDefault}>+</button>
-                <button onClick={() => {addOperator(event, '-')}} onKeyPress={preventDefault}>-</button>
-                <button onClick={() => {addOperator(event, '*')}} onKeyPress={preventDefault}>*</button>
-                <button onClick={() => {addOperator(event, '/')}} onKeyPress={preventDefault}>÷</button>
-                <button onClick={() => {addDigit(event, '1')}}  onKeyPress={preventDefault}>1</button>
-                <button onClick={() => {addDigit(event, '2')}} onKeyPress={preventDefault}>2</button>
-                <button onClick={() => {addDigit(event, '3')}} onKeyPress={preventDefault}>3</button>
+              <button onClick={deleteInput}>C E</button>
+               <button onClick={() => {addDigit(event, '(')}}  onKeyPress={preventDefault}>(</button>
+                <button onClick={() => {addDigit(event, ')')}}  onKeyPress={preventDefault}>)</button>
+                
+               
+                <button className="operator" onClick={() => {addOperator(event, '*')}} onKeyPress={preventDefault}>*</button>
+                <button onClick={() => {addDigit(event, '7')}} onKeyPress={preventDefault}>7</button>
+                <button onClick={() => {addDigit(event, '8')}} onKeyPress={preventDefault}>8</button>
+                <button onClick={() => {addDigit(event, '9')}} onKeyPress={preventDefault}>9</button> 
+                <button className="operator" onClick={() => {addOperator(event, '/')}} onKeyPress={preventDefault}>÷</button>
                 <button onClick={() => {addDigit(event, '4')}} onKeyPress={preventDefault}>4</button>
                 <button onClick={() => {addDigit(event, '5')}} onKeyPress={preventDefault}>5</button>
                 <button onClick={() => {addDigit(event, '6')}} onKeyPress={preventDefault}>6</button>
-                <button onClick={() => {addDigit(event, '7')}} onKeyPress={preventDefault}>7</button>
-                <button onClick={() => {addDigit(event, '8')}} onKeyPress={preventDefault}>8</button>
-                <button onClick={() => {addDigit(event, '9')}} onKeyPress={preventDefault}>9</button>
-                <button onClick={deleteInput}>del</button>
+                <button className="operator" onClick={() => {addOperator(event, '+')}} onKeyPress={preventDefault}>+</button>
+                <button onClick={() => {addDigit(event, '1')}}  onKeyPress={preventDefault}>1</button>
+                <button onClick={() => {addDigit(event, '2')}} onKeyPress={preventDefault}>2</button>
+                <button onClick={() => {addDigit(event, '3')}} onKeyPress={preventDefault}>3</button>
+                <button className="operator" onClick={() => {addOperator(event, '-')}} onKeyPress={preventDefault}>-</button>
+                <button onClick={() => {addDigit(event, '0')}}  onKeyPress={preventDefault}>0</button>
+                <button onClick={() => {addDigit(event, '.')}}  onKeyPress={preventDefault}>.</button>
+                <button onClick={() => {addDigit(event, '%')}}  onKeyPress={preventDefault}>%</button>
+                <button className="operator" onClick={submitInput}type="submit">=</button>
                 <button onClick={clearInput}>AC</button>
-                <button onClick={submitInput}type="submit">=</button>
+              
+                
+                
+                
+                
+                
               </div>
             </div>
           </form>
